8cfc51a43b5c0458d44a6b249be444e74fb6ddd1
dobbi_free <gangb6444@gmail.com>
2022-08-11 15:17:01 +0300

    small_fixes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { Typography } from "@mui/material";
 
 export const Home = () => {
-  return (
-    <>
-      <Typography>Hello</Typography>
-    </>
-  );
+  return <Typography>Hello</Typography>;
 };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 };
