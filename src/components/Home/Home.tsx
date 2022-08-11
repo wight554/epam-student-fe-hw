f0303f188fa527599cb9b6544258643726d25636
Oleksandr Poberezhnyi <68477535+dobbi-free@users.noreply.github.com>
2022-08-11 15:25:03 +0300

    Issue #3. Add react router dom for routing (#9)
    * install_mui_packages
    
    * create_mui_theme
    
    * add_react_router
    
    * remove_redundant_css
    
    * remove_redundant_font_package
    
    * add_font_to_index.html
    
    * resolve_merge_conflicts
    
    * fix_export_home_component
    
    * small_fixes

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const Home = () => {
+  return <Typography>Hello</Typography>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
