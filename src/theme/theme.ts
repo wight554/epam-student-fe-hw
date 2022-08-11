e25de3662f6365bd0640aef4ae9bef294caf8353
Volodymyr Zhdanov <wight554@gmail.com>
2022-08-11 14:04:36 +0300

    Add commands for lint/format and run formatter (#8)

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,9 +1,7 @@
-import {createTheme} from '@mui/material';
+import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    typography: {
-        fontFamily: [
-            'Roboto',
-            'sans-serif'
-        ].join(','),
-    },});
+  typography: {
+    fontFamily: ["Roboto", "sans-serif"].join(","),
+  },
+});
