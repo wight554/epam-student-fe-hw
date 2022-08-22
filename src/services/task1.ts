de4973630020b03fef1a3c77a6cfa797cdfcb4b4
dobbi_free <gangb6444@gmail.com>
2022-08-22 18:09:11 +0300

    move base url to separate file

--- src/services/task1.ts
+++ src/services/task1.ts
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IFile, IFileFormBody } from "../types";
-
-const BASE_URL = "http://localhost:8080";
+import { BASE_URL } from "../constants/api";
 
 export const task1Api = createApi({
   reducerPath: "task1Api",
