9f74783ea9e0cdc44338c64edf1129927251e54b
Oleksandr Poberezhnyi <68477535+dobbi-free@users.noreply.github.com>
2022-08-23 23:39:46 +0300

    6 add optional criteria fe implementation for be task 1 (#15)
    * create base url const
    
    * implement edite and delete for file
    
    * implement edit and delete for file
    
    * refactor
    
    * add refetching by tags
    
    * fix refetch tags
    
    * move base url to separate file
    
    * add refetch tags to delete query
    
    * refactor task1.ts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
 import { useNavigate } from "react-router-dom";
 import { Grid } from "@mui/material";
 
--- src/services/task1.ts
+++ src/services/task1.ts
@@ -1,18 +1,33 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IFile, IFileFormBody } from "../types";
+import { BASE_URL } from "../constants/api";
 
 export const task1Api = createApi({
   reducerPath: "task1Api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
+    baseUrl: `${BASE_URL}`,
   }),
+  tagTypes: ["File"],
   endpoints: (builder) => ({
     getFiles: builder.query<string[], void>({
       query: () => "/api/v1/files",
       transformResponse: ({ files }) => files,
+      providesTags: (result, error, arg) =>
+        result
+          ? [
+              ...result.map(
+                (filename) => ({ type: "File" as const, id: filename }),
+                "File"
+              ),
+            ]
+          : ["File"],
     }),
     getFileByName: builder.query<IFile, string>({
       query: (filename) => `/api/v1/files/${filename}`,
+      providesTags: (result, error, arg) =>
+        result
+          ? [{ type: "File" as const, id: result.filename }, "File"]
+          : ["File"],
     }),
     createFile: builder.mutation<void, IFileFormBody>({
       query: (file) => ({
@@ -21,6 +36,23 @@ export const task1Api = createApi({
         body: file,
       }),
     }),
+    changeFile: builder.mutation<void, Pick<IFile, "filename" | "content">>({
+      query: ({ filename, content }) => ({
+        url: `/api/v1/files/${filename}`,
+        method: "PUT",
+        body: { content },
+      }),
+      invalidatesTags: (result, error, { filename }) => [
+        { type: "File", id: filename },
+      ],
+    }),
+    deleteFile: builder.mutation<void, string>({
+      query: (filename) => ({
+        url: `/api/v1/files/${filename}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "File", id: arg }],
+    }),
   }),
 });
 
@@ -28,4 +60,6 @@ export const {
   useGetFilesQuery,
   useGetFileByNameQuery,
   useCreateFileMutation,
+  useChangeFileMutation,
+  useDeleteFileMutation,
 } = task1Api;
