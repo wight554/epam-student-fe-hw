b4abaeaf6445378268f5d598a9125948d9fd1ad5
Oleksandr Poberezhnyi <68477535+dobbi-free@users.noreply.github.com>
2022-08-31 15:59:33 +0300

    add proxy (#19)
    * add proxy
    
    * refactor vite.config
    
    * refactor vite.config
    
    * add node types
    
    * add cross-env and dev:local script
    
    * move cross-env to dev deps

--- src/api/baseQuery.ts
+++ src/api/baseQuery.ts
@@ -1,8 +1,6 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
-import { BASE_URL } from "../constants/api";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("AUTH_TOKEN");
 
