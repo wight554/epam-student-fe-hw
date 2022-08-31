decb1eb204daba004d0290dc677f849863266e31
dobbi_free <gangb6444@gmail.com>
2022-08-31 14:55:54 +0300

    add node types

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
-
   const env = loadEnv(mode, process.cwd(), "");
 
   return defineConfig({
